5535efa924590f325019270d9031eeed54fce37f
Tina Quach <quacht@mit.edu>
2015-07-28 17:57:58 -0700

    renamed the various outputs of the indel realignment and marking of duplicates for easier file management. commented out the temp direcotry clean up and removal of readgroups from output sam.

--- myMtoolbox.sh
+++ myMtoolbox.sh
@@ -22,35 +22,35 @@ samtoolsexe=/gpfs/group/stsi/methods/variant_calling/bwa_GATK/bin/samtools
 echo ""
 echo "##### REALIGNING KNOWN INDELS WITH GATK INDELREALIGNER..."
 echo ""
-for i in $(ls *ID18_Father_exome_mtExtractremap.csort.bam); do echo $i
-rglibname="$(echo "${i}.rg.bam" | sed 's/_mtExtractremap.csort.bam.rg.bam//')"
+for i in $(ls *ID18_Father_exome_mtExtractremap.csort.bam); do 
+echo "Currently working with ${i}..."
+sampleName="$(echo "${i}.rg.bam" | sed 's/_mtExtractremap.csort.bam.rg.bam//')"
 echo "Adding read groups to the bam files";
 java -Xmx2g \
 -Djava.io.tmpdir=`pwd`/tmp \
 -jar /opt/applications/picard/current/AddOrReplaceReadGroups.jar \
 INPUT="${i}" \
-OUTPUT="${i}.rg.bam" \
+OUTPUT="${sampleName}.rg.bam" \
 SORT_ORDER=coordinate \
-RGID="$rglibname" \
-RGLB="$rglibname" \
+RGID="$sampleName" \
+RGLB="$sampleName" \
 RGPL="ILLUMINA" \
 RGPU="L00" \
-RGSM="${rglibname}_L00" ;
+RGSM="${sampleName}_L00" ;
 
 echo "..."
-samtools index "${i}.rg.bam"
-echo "Currently working with ${i}"
+samtools index "${sampleName}.rg.bam"
 
 #java -jar /opt/applications/picard/current/CreateSequenceDictionary.jar R=/gpfs/home/quacht/ID18exome/rCRS.fasta O=/gpfs/home/quacht/ID18exome/rCRS.dict
 
-echo "Realigning known indels for file" ${i} "using" ${mtoolbox_folder}"data/MITOMAP_HMTDB_known_indels.vcf as reference..."
+echo "Realigning known indels for file ${i} using ${mtoolbox_folder} data/MITOMAP_HMTDB_known_indels.vcf as reference..."
 java -Xmx4g \
 -Djava.io.tmpdir=`pwd`/tmp \
 -jar /opt/applications/gatk/3.3-0/GenomeAnalysisTK.jar \
 -T IndelRealigner \
 -R ${mtoolbox_folder}/data/chr${ref}.fa \
--I "${i}.rg.bam" \
--o "${i}.rgmtRmCsort.realigned.bam" \
+-I "${sampleName}.rg.bam" \
+-o "${sampleName}.rg.ra.bam" \
 -targetIntervals ${mtoolbox_folder}data/intervals_file_RCRS.list \
 -known ${mtoolbox_folder}data/MITOMAP_HMTDB_known_indels_RCRS.vcf \
 -compress 0;
@@ -59,32 +59,32 @@ echo ""
 echo "##### ELIMINATING PCR DUPLICATES WITH PICARDTOOLS MARKDUPLICATES..."
 echo "" 
 java -Xmx4g \
--Djava.io.tmpdir=`pwd`/tmp \
+-Djava.io.tmpdir="$(pwd)/tmp" \
 -jar /opt/applications/picard/current/MarkDuplicates.jar \
-INPUT="${i}.rgmtRmCsort.realigned.bam" \
-OUTPUT="${i}.rgmtRmCsort.realigned.marked.bam" \
-METRICS_FILE="${i}metrics.txt" \
+INPUT="${sampleName}.rg.ra.bam" \
+OUTPUT="${sampleName}.rg.ra.marked.bam" \
+METRICS_FILE="${sampleName}-metrics.txt" \
 ASSUME_SORTED=true \
 REMOVE_DUPLICATES=true \
 TMP_DIR=`pwd`/tmp; done
 
 
-for i in $(ls *rgmtRmCsort.realigned.marked.bam); do
-INPUT="${i}"
-print=$INPUT
+for i in $(ls *rg.ra.marked.bam); do
+echo "Converting ${i} to sam file..."
 java -Xmx4g \
 -Djava.io.tmpdir=`pwd`/tmp \
 -jar ${externaltoolsfolder}SamFormatConverter.jar \
 INPUT="${i}" \
 OUTPUT="${i}.marked.sam" \
-TMP_DIR=`pwd`/tmp;
-#not sure what the following command does
-grep -v "^@" *marked.sam > ${i}.OUT2.sam
+TMP_DIR="`pwd`/tmp";
+#commented out the following command (originally from MtoolBox, since it gave me errors during future bam analysis)
+#grep -v "^@" *marked.sam > ${i}.OUT2.sam
 done
 
+#commented out the follwing section because I wanted to keep viewing the files while testing.
 #mkdir MarkTmp
-#mv *.rgmtRmCsort.realigned.bam MarkTmp
-#mv *.rgmtRmCsort.realigned.marked.bam MarkTmp
+#mv *.ra.bam MarkTmp
+#mv *.ra.marked.bam MarkTmp
 #mv *marked.bam.marked.sam MarkTmp
 #tar -czf MarkTmp.tar.gz MarkTmp
 #rm -R MarkTmp
