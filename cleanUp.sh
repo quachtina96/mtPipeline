4940934ad54b6b5d0e86b122f9093b0e8c776f94
Tina Quach <quacht@mit.edu>
2015-08-03 14:01:01 -0700

    fixed cleanUp to run cleanly and output print statments

--- cleanUp.sh
+++ cleanUp.sh
@@ -11,7 +11,7 @@ USAGE="""This script takes in the path to a sample directory and organizes the '
 		-i option REQUIRED.
 
 		Four directories are created in your sample directory:
-		1) partbam (contains all the part.bam and part.bam.bai files)
+		1) partbam (contains all the part.bam and part.bam.bai files & the indexed exome.bam file that results from the merging of the parts)
 		2) mtExtract (contains all the files created (excluding results) during the initial coverage analysis in simplepipe.py)
 		3) myMtoolbox_out (contains all the files created through myMtoolbox.sh except for results)
 		4) results (contains all relevant results to be seen by the user)
@@ -49,7 +49,9 @@ partBamDir="${pathToSampleDir}/partbam/"
 mkdir $partBamDir
 mv *part.bam $partBamDir
 mv *part.bam.bai $partBamDir
-echo "/partbam holds part.bam files and their indexes"
+mv *exome.bam $partBamDir
+mv *exome.bam.bai $partBamDir
+echo "/partbam holds part.bam files and their indexes, and the indexed exome.bam file that results from the merging of the parts"
 
 echo ""
 mtExtractDir="${pathToSampleDir}/chrM/"
@@ -59,11 +61,11 @@ cd $mtExtractDir
 mv *mtExtractremap.pdf ${pathToSampleDir}
 echo "/mtExtract holds the bam/sam/fastq files resulting from coverage analysis"
 
+cd $pathToSampleDir
 echo ""
 myMtoolbox_out="${pathToSampleDir}/myMtoolbox_out"
 mkdir $myMtoolbox_out
-for file in *.rg.*; do
-	mv $file $myMtoolbox_out 
+mv *rg* $myMtoolbox_out 
 echo "/myMtoolbox_out holds the bam/sam outputs of the indel realignment and marking of duplicates"
 
 rm Rplots.pdf
@@ -76,6 +78,7 @@ mkdir $results
 mv *txt $results
 mv *pdf $results
 mv *vcf $results
-echo "/myMtoolbox_out holds the bam/sam outputs of the indel realignment and marking of duplicates"
+mv *contigs.fasta $results
+echo "/results holds the txt, pdf, and vcf outputs of pipeline for the sample"
 
 
